bba8b965ac0543361e421046f9b4d2e35bf98aa7
Jennifer UBALDO <jubaldo@made-f0Ar3s5.clusters.42paris.fr>
2023-12-09 19:07:56 +0100

    data race

--- philo/eat.c
+++ philo/eat.c
@@ -6,13 +6,13 @@
 /*   By: jubaldo <jubaldo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 11:11:03 by jubaldo           #+#    #+#             */
-/*   Updated: 2023/12/08 11:35:57 by jubaldo          ###   ########.fr       */
+/*   Updated: 2023/12/09 18:19:46 by jubaldo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void	take_forks(t_philo *philo)
+void	philo_eat(t_philo *philo)
 {
 	pthread_mutex_lock(philo->r_fork);
 	print_status("has taken a fork", philo, philo->id);
@@ -24,29 +24,14 @@ static void	take_forks(t_philo *philo)
 	}
 	pthread_mutex_lock(philo->l_fork);
 	print_status("has taken a fork", philo, philo->id);
-}
-
-static void	update_meal(t_philo *philo)
-{
+	philo->eat = 1;
+	print_status("is eating", philo, philo->id);
 	pthread_mutex_lock(philo->meal_lock);
 	philo->last_meal = get_timestamp();
 	philo->meals_eaten++;
 	pthread_mutex_unlock(philo->meal_lock);
 	ft_usleep(philo->time_to_eat);
-}
-
-static void	release_forks(t_philo *philo)
-{
+	philo->eat = 0;
 	pthread_mutex_unlock(philo->l_fork);
 	pthread_mutex_unlock(philo->r_fork);
-}
-
-void	philo_eat(t_philo *philo)
-{
-	take_forks(philo);
-	philo->eat = 1;
-	print_status("is eating", philo, philo->id);				
-	update_meal(philo);
-	philo->eat = 0;
-	release_forks(philo);
 }
